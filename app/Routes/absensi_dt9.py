54d05268acf66c776b212a8ac74d4bf65d1189fc
rhan15 <108850827+rhan15@users.noreply.github.com>
2026-05-06 18:41:34 +0700

    COMPARE DT9 TO DT9

--- app/Routes/absensi_dt9.py
+++ app/Routes/absensi_dt9.py
@@ -16,12 +16,13 @@ def absensi_dt9():
 
 def proced_absensi_dt9():
    try:
+    
     print("Job penyebaran DT9 dijalankan")
     # CONFIG FTP
     FTP_HOST = "172.20.28.245"
     FTP_USER = "omiho"
     FTP_PASS = "omihoftp"
-    FTP_DIR  = "/igrcpg/dt9bak/06052016"
+    FTP_DIR = f"/igrcpg/dt9bak/{datetime.now():%d%m}2016"
 
     # CONNECT FTP
     ftp = FTP(FTP_HOST, timeout=30)
@@ -32,31 +33,21 @@ def proced_absensi_dt9():
     # AMBIL LIST FILE
     files = ftp.nlst()
 
-    print("List file di FTP:")
-    dt9Files = [f for f in files if f.startswith("DT9")]
+    FTP_dt9Files = [f for f in files if f.startswith("DT9")]
 
-    # for f in dt9Files:
-    #     print(f)
-    listKodeToko = [f.replace(".", "")[-4:] for f in dt9Files]  
-    # for r in listKodeToko:
-    #     print(r)
-
-    PathFolderToko = Path(os.getenv("PUBLIC_DIR"))
-    listKodeTokoSFTP = [
-        f.name for f in PathFolderToko.iterdir() if f.is_dir()
+    PathFolderToko = Path(os.getenv("DIRECTORY_DT9"))
+    SFTP_dt9Files = [
+        f.name for f in PathFolderToko.iterdir() if f.is_file() and f.name.startswith("DT9")
     ]
-    
-    missing_toko = list(set(listKodeToko) - set(listKodeTokoSFTP))
 
-    for m in missing_toko:
-        print(f"Tidak ada di lokal: {m}")
+    missing_toko = list(set(FTP_dt9Files) - set(SFTP_dt9Files))
 
     return {
         "status": "success",
         "toko_kurang": missing_toko,
         "total_toko_kurang": len(missing_toko),
-        "total_toko_ftp": len(listKodeToko),
-        "total_toko_sftp": len(listKodeTokoSFTP),
+        "total_dt9_ftp": len(FTP_dt9Files),
+        "total_dt9_sftp": len(SFTP_dt9Files),
     }
 
 
